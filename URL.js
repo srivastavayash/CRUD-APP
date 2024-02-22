ca871cb7b1f0372c7d24d56c715086f8e3c96774
Yash Srivastava <srivastavayash017@gmail.com>
2024-02-22 14:19:13 +0530

    pwdhides

--- URL.js
+++ URL.js
@@ -1,2 +1,2 @@
-const URL="mongodb+srv://srivastavayash:jinglebell@cluster0.5ddevao.mongodb.net/public/?retryWrites=true&w=majority";
+const URL="mongodb+srv://srivastavayash:pwd@cluster0.5ddevao.mongodb.net/public/?retryWrites=true&w=majority";
 module.exports=URL;
